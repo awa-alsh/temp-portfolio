6c469b5b6842a1523a9149d03c1d4f5271b77775
awa-alsh <aalshami.21.7@gmail.com>
2025-04-13 00:14:51 +0100

    Decrease project teaser arrow size in large desktop view

--- app/components/ProjectTeaser.tsx
+++ app/components/ProjectTeaser.tsx
@@ -71,7 +71,7 @@ export default function ProjectTeaser({
           </div>
 
           <div className="flex items-center space-x-2 mt-4 lg:mt-8 xl:mt-12">
-            <span className={`font-medium text-l lg:text-xl xl:text-2xl ${isUnavailable ? 'text-text-secondary' : 'text-accent'}`}>
+            <span className={`font-medium text-l lg:text-xl ${isUnavailable ? 'text-text-secondary' : 'text-accent'}`}>
               {isUnavailable ? 'This project is unavailable to view' : 'View Project'}
             </span>
             {isUnavailable ? (
@@ -89,7 +89,7 @@ export default function ProjectTeaser({
                 />
               </svg>
             ) : (
-              <svg width="24" height="8" viewBox="0 0 16 8" fill="none" xmlns="http://www.w3.org/2000/svg" className="arrow-icon w-[40px] lg:w-[50px] xl:w-[60px] h-auto">
+              <svg width="24" height="8" viewBox="0 0 16 8" fill="none" xmlns="http://www.w3.org/2000/svg" className="arrow-icon w-[40px] lg:w-[50px] h-auto">
                 <path d="M15 4H4V1" stroke="#00ff9d" />
                 <path d="M14.5 4H3.5H0" stroke="#00ff9d" />
                 <path d="M15.8536 4.35355C16.0488 4.15829 16.0488 3.84171 15.8536 3.64645L12.6716 0.464466C12.4763 0.269204 12.1597 0.269204 11.9645 0.464466C11.7692 0.659728 11.7692 0.976311 11.9645 1.17157L14.7929 4L11.9645 6.82843C11.7692 7.02369 11.7692 7.34027 11.9645 7.53553C12.1597 7.7308 12.4763 7.7308 12.6716 7.53553L15.8536 4.35355ZM15 4.5L15.5 4.5L15.5 3.5L15 3.5L15 4.5Z" fill="#00ff9d"/>
